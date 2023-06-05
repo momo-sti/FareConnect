44eb4672c45738d299118f9c05b4598c758f7e83
momo-sti <momoka1112sti@gmail.com>
2023-06-05 12:15:55 +0900

    FIX:マイグレーションファイル削除

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,24 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_26_062251) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "authentications", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "provider", null: false
+    t.string "uid", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["provider", "uid"], name: "index_authentications_on_provider_and_uid"
+    t.index ["user_id"], name: "index_authentications_on_user_id"
+  end
+
+  create_table "places", force: :cascade do |t|
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "avatar"
