7bd59e88fe34f24a653d1e0b6a53c32f04db62b4
momo-sti <momoka1112sti@gmail.com>
2023-06-08 17:24:41 +0900

    FIX:DirectionsAPIからデータ持ってくる（途中）

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  resources :tops
-  resources :calculations
-  
-  root 'tops#index'
+  resources :tops do
+    get :top, on: :collection
+  end
+  resources :calculations do
+    get :result, on: :collection
+    post :datasave, on: :collection
+  end
+
+  root 'tops#top'
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
