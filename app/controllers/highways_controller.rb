53e549f71769b5d1d7ba9e89c96cee98f907ec7f
momo-sti <momoka1112sti@gmail.com>
2023-06-15 12:52:36 +0900

    ADD:高速料金の検索条件追加

--- app/controllers/highways_controller.rb
+++ app/controllers/highways_controller.rb
@@ -6,9 +6,16 @@ class HighwaysController < ApplicationController
   def create
     start_place = CGI.escape(params[:start_place])
     arrive_place = CGI.escape(params[:arrive_place])
-    car_type = CGI.escape(params[:car_type])
+    car_type = params[:car_type]
+    date = params[:date]
+    year, month, day = date.split("-")
+    #time(？i) = time_selectのパラメータ形式
+    hour = params["time(4i)"]
+    min = params["time(5i)"]
+    kind = params[:kind]
 
-    redirect_to "https://www.driveplaza.com/dp/SearchQuick?startArrive=true&startPlaceKana=#{start_place}&arrivePlaceKana=#{arrive_place}&carType=#{car_type}", allow_other_host: true
+    #ドラぷらサイトに遷移
+    redirect_to "https://www.driveplaza.com/dp/SearchQuick?startPlaceKana=#{start_place}&arrivePlaceKana=#{arrive_place}&searchHour=#{hour}&searchMinute=#{min}&kind=#{kind}&keiyuPlaceKana=&keiyuPlaceKana2=&keiyuPlaceKana3=&searchYear=#{year}&searchMonth=#{month}&searchDay=#{day}&roadType1=off&roadType2=off&roadType=15&carType=1&priority=2&selectickindflg=0", allow_other_host: true
   end
   
 end
